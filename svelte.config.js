bc4fdebfd0bd7f20b0011a4c0fd7b406ac59d939
jonny <joniamhandy@gmail.com>
2023-09-18 17:40:49 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@ivorgri/sveltekit-pluggable-static-adapter';
+import replaceExternalImages from '@ivorgri/sveltekit-pluggable-static-adapter-external-image-plugin';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			afterPrerenderCallback: async (builder, pages, assets) => {
+				await replaceExternalImages("https://zu-spaet-fuer-die-welle.pockethost.io/api/",builder,pages,assets)
+			},
+		})
 	},
 	preprocess: vitePreprocess()
 };
