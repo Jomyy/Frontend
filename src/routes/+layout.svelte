427676643c14413eb7bb63a77624beed9c988fcf
jonny <joniamhandy@gmail.com>
2023-09-18 17:26:18 +0200

    hm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,9 @@
 		navOpen = !navOpen;
 		console.log(navOpen);
 	}
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <nav
