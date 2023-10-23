1ec49b69ef8308d20b89c334a78671c64af4f6b3
jonny <joniamhandy@gmail.com>
2023-10-23 13:15:43 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,8 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			split: true
+		})
 	}
 }
