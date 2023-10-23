5f529f212fbb3103f147144f5a4c4232f318023d
Jonny <62953110+Jomyy@users.noreply.github.com>
2023-10-23 09:47:16 +0200

    Update and rename +page.js to +page.server.js

--- src/routes/releases/+page.server.js
+++ src/routes/releases/+page.server.js
@@ -1,6 +1,12 @@
 import { apiURL } from '$lib';
 export const prerender = true;
 export const csr = false;
+export const config = {
+  isr: {
+    expiration: 60,
+    bypassToken: 'REPLACE_ME_WITH_SECRET_VALUE',
+  },
+};
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
     const res = await fetch(apiURL + "collections/releases/records")
