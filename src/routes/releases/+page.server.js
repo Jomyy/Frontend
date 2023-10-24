d217a6edc55a05585332ecf6e32aefdb5fcc4d4a
Jonny <62953110+Jomyy@users.noreply.github.com>
2023-10-24 08:46:12 +0200

    ljhjh

--- src/routes/releases/+page.server.js
+++ src/routes/releases/+page.server.js
@@ -9,10 +9,10 @@ export const config = {
 export async function load({ fetch, params }) {
   const res = await fetch(apiURL + "collections/releases/records")
   const releases = await res.json()
-  const orderedReleases = releases.items.sort(function(a,b){
+  const orderedReleases = releases["items"].sort(function(a,b){
     // Turn your strings into dates, and then subtract them
     // to get a value that is either negative, positive, or zero.
-    return b.Date - a.Date;
+    return b["Date"] - a["Date"];
   })
   return {orderedReleases}
 
