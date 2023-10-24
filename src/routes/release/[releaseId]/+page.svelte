9d17b440d5f4db0b7831c3be507de61684bcade2
jonny <joniamhandy@gmail.com>
2023-10-24 11:11:29 +0200

    fixfix

--- src/routes/release/[releaseId]/+page.svelte
+++ src/routes/release/[releaseId]/+page.svelte
@@ -61,7 +61,9 @@
 	</div>
 	<div class=" p-4">
 		<p class="text-white font-sans text-center text-2xl pb-4">Available Now On:</p>
-		<div class="grid grid-cols-3 md:grid-cols-5 justify-center items-center justify-items-center">
+		<div
+			class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 justify-center items-center justify-items-center"
+		>
 			<div class="p-4">
 				<a
 					class="flex flex-col p-8 rounded-xl w-min justify-center items-center aspect-square h-32 bg-black"
