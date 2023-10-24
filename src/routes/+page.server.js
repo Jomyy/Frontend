fe10a2b288b0638ded91c3ba2b83e561b0525441
jonny <joniamhandy@gmail.com>
2023-10-24 13:08:05 +0200

    ated

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -10,12 +10,13 @@ export async function load({ fetch, params }) {
     const resRecords = await fetch(apiURL + "collections/releases/records")
     const releases = await resRecords.json()
 
-    const newestRelease = releases["items"].sort(function (/** @type {{ Date: string; }} */ a, /** @type {{ Date: string; }} */ b) {
-        // Turn your strings into dates, and then subtract them
-        // to get a value that is either negative, positive, or zero.
+    //const newestRelease = releases["items"].sort(function (/** @type {{ Date: string; }} */ a, /** @type {{ Date: string; }} */ b) {
+    // Turn your strings into dates, and then subtract them
+    // to get a value that is either negative, positive, or zero.
 
-        return Date.parse(b.Date) - Date.parse(a.Date)
-    })[0]
+    //  return Date.parse(b.Date) - Date.parse(a.Date)
+    //})[0]
+    const newestRelease = {}
     return { newestRelease }
 
 
